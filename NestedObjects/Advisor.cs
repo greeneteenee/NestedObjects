cc03568bb2f1e147afbeb4d65232e3e0795d6335
Tina Greene <baby_phat03@msn.com>
2019-01-14 14:36:26 -0800

    Created an advisor class

---
+++ NestedObjects/Advisor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedObjects
+{
+    /// <summary>
+    /// This class represents a student advisor
+    /// </summary>
+    class Advisor
+    {
+        public string FullName { get; set; }
+
+        public string Email { get; set; }
+
+        
+    }
+}
