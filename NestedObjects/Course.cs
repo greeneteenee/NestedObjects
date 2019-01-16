b45d72e9954a5ba729bb5bdafa9b7b1b85419982
Tina Greene <baby_phat03@msn.com>
2019-01-16 14:12:26 -0800

    Added basic course class with nested instructor class

---
+++ NestedObjects/Course.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedObjects
+{
+    class Course
+    {
+        /// <summary>
+        /// Course title 
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Number of credit for course
+        /// </summary>
+        public byte Credits { get; set; }
+
+        /// <summary>
+        /// Course description
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Instructor who teaches course
+        /// </summary>
+        public Instructor CourseInstructor { get; set; }
+
+    }
+}
