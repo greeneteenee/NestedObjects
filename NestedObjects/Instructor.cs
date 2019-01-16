c4f09966dab8af253130ff39f76e5a8f6c7858d7
Tina Greene <baby_phat03@msn.com>
2019-01-16 13:27:01 -0800

    Added documentatin to Instructor class

--- NestedObjects/Instructor.cs
+++ NestedObjects/Instructor.cs
@@ -9,17 +9,17 @@ namespace NestedObjects
     class Instructor
     {
         /// <summary>
-        /// Property: First Last name of instructor
+        /// Property: First Last name of instructor ex. Joseph Ortiz
         /// </summary>
         public string FullName { get; set; }
 
         /// <summary>
-        /// Property: Instructor email address
+        /// Property: Instructor work email address
         /// </summary>
         public string Email { get; set; }
 
         /// <summary>
-        /// Property: Instructor phone number
+        /// Property: Instructor office phone number
         /// </summary>
         public string Phone { get; set; }
 
