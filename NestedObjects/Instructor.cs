cabe990d0dc9cb4ef8ee9465b2a74b5439078e92
Tina Greene <baby_phat03@msn.com>
2019-01-16 13:21:59 -0800

    Added basic instructor class

---
+++ NestedObjects/Instructor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedObjects
+{
+    class Instructor
+    {
+        /// <summary>
+        /// Property: First Last name of instructor
+        /// </summary>
+        public string FullName { get; set; }
+
+        /// <summary>
+        /// Property: Instructor email address
+        /// </summary>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Property: Instructor phone number
+        /// </summary>
+        public string Phone { get; set; }
+
+    }
+}
